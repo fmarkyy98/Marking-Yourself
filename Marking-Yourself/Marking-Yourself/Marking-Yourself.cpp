72100184989b2f09ca4095f50715cbd8d1458e0c
Fekete Márk <fmark98@gmail.com>
2019-09-18 17:19:08 +0200

    Created the intTryParse() function that works similar as the int.TryParse() in C#.

--- Marking-Yourself/Marking-Yourself/Marking-Yourself.cpp
+++ Marking-Yourself/Marking-Yourself/Marking-Yourself.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+bool intTryParse(string s, int& out)
+{
+	bool result = true;
+	int i = 0;
+	while (i < s.length() && result)
+	{
+		if (!isdigit(s[i]))
+		{
+			result = false;
+		}
+		++i;
+	}
+	if (result) { out = stoi(s); }
+	return result;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string input;
+	int mark = -1;
+	do
+	{
+		cout << "As mark type a number between 1 an 5!\nFor Exite type nothing.\n";
+		cin >> input;
+	} while (!intTryParse(input, mark) || !(mark >= 1 && mark <= 5));
+	cout << "Thankyou for the selfreflection, you got" << mark << ".\n";
+
+	//cin >> input;//Get the injput.
+	//cin.fail(); //returns with a boolean that reprezents the succes of the cin.
+	//cin.clear(); //reset the False value of .fail.
+	//cin.ignore(); //ignore the current stream until the next enter.
+	//cin.peek(); // peeks the next element of the input.
 }
